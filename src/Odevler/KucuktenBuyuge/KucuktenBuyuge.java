d37a0fac4c052cef10cf3796092a360755659340
Burak <burockcan2309@gmail.com>
2021-09-11 17:12:42 +0300

    Kucukten Buyuge Sıralama

---
+++ src/Odevler/KucuktenBuyuge/KucuktenBuyuge.java
@@ -0,0 +1,40 @@
+package Odevler.KucuktenBuyuge;
+
+import java.util.Scanner;
+
+public class KucuktenBuyuge {
+
+    public static void main(String[] args) {
+
+        Scanner klavye=new Scanner(System.in);
+        System.out.print("ilk sayiyi giriniz: ");
+        int a = klavye.nextInt();
+        System.out.print("İkinci sayiyi giriniz: ");
+        int b = klavye.nextInt();
+        System.out.print("Üçüncü sayiyi giriniz: ");
+        int c = klavye.nextInt();
+
+        if (a >= b){
+            if (b >= c)
+                System.out.print("Üç sayının sıralaması: " + c + " "+ b + " " + a);
+
+            else if  (c >= a)
+                System.out.print("Üç sayının sıralaması: " + b + " "+ a + " " + c);
+
+            else if (a > c)
+                System.out.print("Üç sayının sıralaması: " + b + " " + c + " " + a);
+        }
+
+        if (b > a){
+            if (c >= b)
+                System.out.print("Üç sayının sıralaması: " + a + " " + b + " "+ c);
+            else if (c >= a)
+                System.out.print("Üç sayının sıralaması: " + a + " " + c + " " + b);
+            else if (a > c)
+                System.out.print("Üç sayının sıralaması: " + c + " " + a + " " + b);
+        }
+
+
+    }
+
+}
